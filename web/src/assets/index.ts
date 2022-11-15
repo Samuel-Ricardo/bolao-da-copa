6f20e8fa352c8c2b4acf5fb4eb86c2686659e192
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-11-15 17:37:57 -0300

    :sparkles: update: index - assets

--- web/src/assets/index.ts
+++ web/src/assets/index.ts
@@ -1,3 +1,4 @@
 export * as logo from './logo.svg'
+export * as check_icosn from './icon-check.svg'
 export * as avatars_example from './users-avatar-example.png'
 export * as app_preview from './app-nlw-copa-preview.png'
