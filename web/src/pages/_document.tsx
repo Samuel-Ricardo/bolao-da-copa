83ef40407a404d81abd0933c7fcaab7bfca018de
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-11-15 17:02:21 -0300

    :sparkles: create: base - document

---
+++ web/src/pages/_document.tsx
@@ -0,0 +1,14 @@
+import {Head, Html} from 'next/document';
+
+export default function Document() {
+ return (
+    <Html>
+        <Head>
+
+        </Head>
+        <body>
+            
+        </body>
+    </Html>
+ )   
+}
