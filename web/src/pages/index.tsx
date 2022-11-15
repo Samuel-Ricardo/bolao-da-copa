4af471848784a9d9ac16e54e6d4d6ae36112c489
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-11-15 17:18:08 -0300

    :sparkles: create: fun - createPool (pages/home)

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,10 +1,33 @@
+import { FormEvent, useState } from "react";
+import { api } from "../lib/axios";
+
 interface IHomeProps {
   poolCount: number;
   guessCount: number;
   userCount: number;
 }
 
-export default function Home(props: HomeProps) {
+export default function Home(props: IHomeProps) {
+  const [poolTitle, setPoolTitle] = useState('')
+
+  async function createPool(event: FormEvent){
+    event.preventDefault();
+
+    try {
+      const response = await api.post('/pools', {title:poolTitle})
+
+      const {code} = response.data;
+      await navigator.clipboard.writeText(code);
+
+      alert('Bolão criado com sucesso, o código foi copoado para a área de transferencia! ;)')
+
+      setPoolTitle('')
+    } catch (err) {
+      console.error(err);
+      alert('Falha ao criar o bolão :/  \n\n tente novamente! :D')
+    }
+  }
+
   return (
     <div>
       <h1>Hello World :D</h1>
