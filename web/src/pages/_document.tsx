39bd5a60814d29cb92929a80bbc5a505789322e6
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-11-15 17:07:05 -0300

    :building_construction: update: base - document (head)

--- web/src/pages/_document.tsx
+++ web/src/pages/_document.tsx
@@ -4,7 +4,9 @@ export default function Document() {
  return (
     <Html>
         <Head>
-
+            <link rel="preconnect" href="https://fonts.googleapis.com"/>
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin=""/>
+            <link rel="preconnect" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"/>
         </Head>
         <body>
             
